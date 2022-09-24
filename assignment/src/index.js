3f5f3130c0fee03f1b3baba56272a1ad3cea82a0
Hisham <97hisham@gmail.com>
2022-09-25 02:35:54 +0530

    'first'

---
+++ assignment/src/index.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import './index.css';
+import Products from './components/products/Products';
+
+
+const root = ReactDOM.createRoot(document.getElementById('root'));
+
+root.render(
+    
+  <Products/> 
+
+);
